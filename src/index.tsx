8b24ee3349a5f947c177a4b274b29af97d52127a
baptiste-boimard <bouketin27@gmail.com>
2023-10-03 11:37:41 +0200

    installation de l'environnement

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
+
+import store from './store';
+import App from './App';
+
+import './index.css';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App /> 
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
+);
+
+reportWebVitals();
---
+++ src/reducers/index.tsx
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+
+// ==-- IMPORT SLICE--==
+
+
+const rootReducer = combineReducers({
+
+});
+
+export default rootReducer;
