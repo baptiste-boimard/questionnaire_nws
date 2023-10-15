d68daa794617442f388adcf32be3e261426a9487
baptiste-boimard <bouketin27@gmail.com>
2023-10-15 19:25:28 +0200

    Mise a jour des cors-allow-origin

--- back/src/index.ts
+++ back/src/index.ts
@@ -33,7 +33,7 @@ app.use(express.json({extended: true}));
 // router.options('*', cors(options));
 
 app.use((req: Request ,res: Response ,next: NextFunction) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'http://51.75.133.175:3000');
   res.header(`Access-Control-Allow-Methods`, `GET, PATCH, PUT, POST, DELETE, OPTIONS, HEAD`);
   res.header(`Access-Control-Allow-Headers`, `Origin, X-Requested-With,Content-Type,Authorization, Accept`);
   // A controler, je n'arrive pas à le typer correctement à part avec accept au lieu de true
@@ -47,7 +47,7 @@ app.use(express.static('docs'));
 // Middleware pour attraper les erreurs
 app.use(handleError);
 
-app.listen(PORT,'0.0.0.0', () => {
+app.listen(PORT,'51.75.133.155', () => {
   console.log(`Listening on http://localhost:${PORT}`);
 });
 
